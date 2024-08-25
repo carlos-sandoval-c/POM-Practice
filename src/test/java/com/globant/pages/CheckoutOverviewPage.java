d189df478a24f1b77baf0a9f26ec2c1e137b5521
Carlos Sandoval <c.sandoval@globant.com>
2024-08-25 04:16:35 -0500

    feat(purchase-flow): implement checkout information page

---
+++ src/test/java/com/globant/pages/CheckoutOverviewPage.java
@@ -0,0 +1,10 @@
+package com.globant.pages;
+
+import com.globant.utils.basePage.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutOverviewPage extends BasePage {
+    public CheckoutOverviewPage(WebDriver driver) {
+        super(driver);
+    }
+}
